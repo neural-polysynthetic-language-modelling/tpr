9c2ef1a26cad4e96ca06dde0796b54e2ca37eea1
Lane Schwartz <dowobeha@gmail.com>
2019-11-06 13:48:32 -0600

    Ready to test morpheme corpus

---
+++ corpus.py
@@ -0,0 +1,113 @@
+import argparse
+import configargparse
+from typing import Iterable, List, MutableSet, Optional
+
+from torch.utils.data.dataset import Dataset
+
+from features import Alphabet
+from morpheme import Morpheme, Morphemes
+from tokenizer import Tokenizer
+
+
+class MorphemeCorpus(Dataset):
+
+    def __init__(self, *,
+                 alphabet: Alphabet,
+                 sentences: Iterable[str],
+                 tokenizer: Tokenizer,
+                 blacklist_char: str,
+                 start_of_morpheme: str, end_of_morpheme: str,
+                 max_graphemes_per_morpheme: Optional[int]):
+
+        morphemes: List[List[str]] = list()
+        processed_morphemes: MutableSet[str] = set()
+
+        for sentence in sentences:
+            for word in tokenizer.words(sentence):
+                if not word.startswith(blacklist_char):
+                    for morpheme in tokenizer.morphemes(word):
+                        if morpheme not in processed_morphemes:
+                            processed_morphemes.add(morpheme)
+                            graphemes = tokenizer.graphemes(morpheme)
+                            if max_graphemes_per_morpheme is None or len(graphemes) <= max_graphemes_per_morpheme:
+                                morphemes.append(graphemes)
+
+        self.morphemes = Morphemes(alphabet=alphabet,
+                                   start_of_morpheme=start_of_morpheme, end_of_morpheme=end_of_morpheme,
+                                   list_of_morphemes=morphemes)
+
+    def __len__(self) -> int:
+        return len(self.morphemes)
+
+    def __getitem__(self, index: int) -> Morpheme:
+        return self.morphemes[index]
+
+    @staticmethod
+    def load(filename: str) -> 'MorphemeCorpus':
+        import pickle
+        with open(filename, 'rb') as pickled_file:
+            return pickle.load(pickled_file)
+
+    def dump(self, filename: str) -> None:
+        import pickle
+        with open(filename, 'wb') as pickled_file:
+            pickle.dump(self, pickled_file)
+
+
+def configure(arguments: List[str]) -> argparse.Namespace:
+
+    p = configargparse.get_argument_parser(description="Construct corpus tokenizer")
+    p.add('-c', '--config', required=False, is_config_file=True, type=str, metavar='FILENAME',
+          help='configuration file')
+
+    p.add('-a', '--alphabet', required=True, type=str, metavar='FILENAME',
+          help='Pickle file containing a Alphabet object')
+
+    p.add('--tokenizer', required=True, type=str, metavar='FILENAME',
+          help='Pickle file containing a Tokenizer object')
+
+    p.add('-b', '--blacklist_character', required=True, type=str, metavar='STRING',
+          help="In the user-provided input file, words that begin with this character will be ignored." +
+               "This symbol should not appear in the alphabet")
+
+    p.add('-m', '--max_characters', required=False, type=int, metavar='N',
+          help='Maximum number of characters allowed per morpheme (not including reserved start and end characters). ' +
+               'If not provided, value will be inferred from longest morpheme in input.')
+
+    p.add('--start_of_morpheme', required=True, type=str, metavar='STRING',
+          help="Reserved symbol to be inserted at the start of each morpheme. This symbol must appear in the alphabet")
+
+    p.add('--end_of_morpheme', required=True, type=str, metavar='STRING',
+          help="Reserved symbol to be inserted at the end of each morpheme. This symbol must appear in the alphabet")
+
+    p.add('-i', '--input_file', required=True, type=str, metavar="FILENAME",
+          help="Input file containing corpus in plain-text format")
+
+    p.add('-o', '--output_file', required=True, type=str, metavar="FILENAME",
+          help="Output file where pickled MorphemeCorpus object will be saved")
+
+    return p.parse_args(args=arguments)
+
+
+def main(args: argparse.Namespace) -> None:
+
+    import pickle
+
+    with open(args.input_file, 'rt') as input_file, open(args.output_file, 'wb') as output_file:
+
+        corpus = MorphemeCorpus(alphabet=Alphabet.load(args.alphabet),
+                                tokenizer=Tokenizer.load(args.tokenizer),
+                                sentences=input_file,
+                                blacklist_char=args.blacklist_char,
+                                max_graphemes_per_morpheme=args.max_graphemes_per_morpheme,
+                                start_of_morpheme=args.start_of_morpheme,
+                                end_of_morpheme=args.end_of_morpheme)
+
+        pickle.dump(corpus, output_file)
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    main(configure(arguments=sys.argv[1:]))
--- morpheme.py
+++ morpheme.py
@@ -1,4 +1,4 @@
-from typing import List, MutableMapping, NamedTuple
+from typing import Any, List, Mapping, NamedTuple
 
 from features import Alphabet, Symbol
 
@@ -11,59 +11,72 @@ class Morpheme(NamedTuple):
         return len(self.graphemes)
 
     def __str__(self) -> str:
-        return "".join(self.graphemes)
+        return Morphemes.list_to_string(self.graphemes, delimiter='')
 
 
 class Morphemes:
 
     def __init__(self, *,
-                 alphabet: Alphabet, max_len: int,
-                 start_of_morpheme: str, end_of_morpheme: str):
+                 alphabet: Alphabet,
+                 start_of_morpheme: str, end_of_morpheme: str,
+                 list_of_morphemes: List[List[str]]):
         if start_of_morpheme not in alphabet or end_of_morpheme not in alphabet:
             raise ValueError(f"Start-of-morpheme symbol {start_of_morpheme}" +
                              f" and end-of-morpheme symbol {end_of_morpheme} must both be present in the alphabet")
         self.alphabet: Alphabet = alphabet
-        self.max_len: int = max_len
+        self.max_len: int = len(max(list_of_morphemes, key=len))
         self.start_of_morpheme: Symbol = alphabet[start_of_morpheme]
         self.end_of_morpheme: Symbol = alphabet[end_of_morpheme]
         self._grapheme_delimiter: str = "\u001F"
-        self._dictionary: MutableMapping[str, Morpheme] = dict()
 
-    def _get_key(self, graphemes: List[str]) -> str:
-        if isinstance(graphemes, list) and self._grapheme_delimiter not in graphemes:
-            return self._grapheme_delimiter.join(graphemes)
-        else:
-            raise ValueError
+        morpheme_symbols: List[List[Symbol]] = [[alphabet[grapheme] for grapheme in graphemes]
+                                                for graphemes in list_of_morphemes]
 
-    def _get_tpr(self, graphemes: List[Symbol]) -> List[List[int]]:
-        if len(graphemes) > self.max_len:
-            raise ValueError(f"Morpheme {str(graphemes)} has length longer than maximum allowed {self.max_len}")
+        tprs: List[List[List[int]]] = [Morphemes.tpr(morpheme, self.max_len,
+                                                     self.start_of_morpheme, self.end_of_morpheme, alphabet.pad)
+                                       for morpheme in morpheme_symbols]
 
-        num_pads = self.max_len - len(graphemes)
+        self.morpheme_list: List[Morpheme] = [Morpheme(graphemes, tpr)
+                                              for (graphemes, tpr) in zip(morpheme_symbols, tprs)]
 
-        symbols: List[Symbol] = ([self.start_of_sequence] +
+        self.morpheme_map: Mapping[str, Morpheme] = {Morphemes.list_to_string(morpheme.graphemes,
+                                                                              self._grapheme_delimiter): morpheme
+                                                     for morpheme in self.morpheme_list}
+
+    @staticmethod
+    def tpr(graphemes: List[Symbol], max_len: int,
+            start_of_sequence: Symbol, end_of_sequence: Symbol, pad: Symbol) -> List[List[int]]:
+        if len(graphemes) > max_len:
+            raise ValueError(f"Morpheme {str(graphemes)} has length longer than maximum allowed {max_len}")
+
+        num_pads = max_len - len(graphemes)
+
+        symbols: List[Symbol] = ([start_of_sequence] +
                                  graphemes +
-                                 [self.end_of_sequence] +
-                                 [self.alphabet.pad] * num_pads)
+                                 [end_of_sequence] +
+                                 [pad] * num_pads)
 
         return [symbol.vector for symbol in symbols]
 
-    def add(self, graphemes: List[str]) -> Morpheme:
-        key: str = self._get_key(graphemes)
-        symbols: List[Symbol] = [self.alphabet[grapheme] for grapheme in graphemes]
-        tpr: List[List[int]] = self._get_tpr(symbols)
-        morpheme = Morpheme(symbols, tpr)
-        self._dictionary[key] = morpheme
-        return morpheme
+    @staticmethod
+    def list_to_string(graphemes: List[Any], delimiter: str) -> str:
+        if isinstance(graphemes, list) and delimiter not in graphemes:
+            return delimiter.join([str(grapheme) for grapheme in graphemes])
+        else:
+            raise ValueError
 
-    def __getitem__(self, graphemes: List[str]) -> Morpheme:
-        key: str = self._get_key(graphemes)
-        return self._dictionary[key]
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.morpheme_list[key]
+        elif isinstance(key, list):
+            key: str = Morphemes.list_to_string(key, self._grapheme_delimiter)
+            return self.morpheme_map[key]
+        else:
+            return ValueError
 
     def __len__(self) -> int:
-        return len(self._dictionary)
+        return len(self.morpheme_list)
 
     def __contains__(self, graphemes: List[str]) -> bool:
-        key: str = self._get_key(graphemes)
-        return key in self._dictionary
-
+        key: str = Morphemes.list_to_string(graphemes, self._grapheme_delimiter)
+        return key in self.morpheme_map
